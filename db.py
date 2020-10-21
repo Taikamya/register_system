9d1b7ab02609dd6eaefd5994016229fbf3d07588
Dimitri Dias Moreira <taikamya@outlook.com>
2020-10-21 01:00:46 -0300

    Update db.py
    Correções pequenas antes de mudar totalmente o DB

--- db.py
+++ db.py
@@ -31,10 +31,12 @@ class ConnectionDB():
         self.conn = sqlite3.connect("register.db")
         self.db_cursor = self.conn.cursor()
 
+        # row -> id // Primary Key, name, course and enrolled -> NOT NULL // Added "enrolled" option // Dimitri Dias Moerira (Taikamya)
         self.db_cursor.execute('''CREATE TABLE IF NOT EXISTS students 
-                                    (row INTEGER PRIMARY KEY AUTOINCREMENT,
-                                    name TEXT,
-                                    course TEXT,
+                                    (id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
+                                    name TEXT NOT NULL,
+                                    course TEXT NOT NULL,
+                                    enrolled INTEGER NOT NULL,
                                     semester INTEGER,
                                     telephone INTEGER,
                                     address TEXT)''')
@@ -42,7 +44,7 @@ class ConnectionDB():
         # self.conn.close() --> Não pode fechar o DB
 
     # Função responsável por manter o painel com os dados atualizados do sistema.
-    def load_one_student(self, number):
+    def load_one_student(self, id):
         result = self.db_cursor.execute('''SELECT * FROM students''')
         self.db_cursor.rowcount(0) # -- Também dá erro, mas deixa funcionar até certo momento.
         # self.tableWidget.setRowCount(0)  --> Isso tinha que estar aqui mesmo? Dá erro.
@@ -54,9 +56,9 @@ class ConnectionDB():
         self.conn.close()
 
     # Função responsável por realizar a pesquisa no sistema.
-    def select_one_student(self, number):
+    def select_one_student(self, id):
         try:
-            result = self.db_cursor.execute("SELECT * FROM students WHERE row =?", str(number)) # Consertada sintaxe // Não pode transformar em str o que é validado como int
+            result = self.db_cursor.execute("SELECT * FROM students WHERE row =?", id) # Consertada sintaxe // Não pode transformar em str o que é validado como int
             row = result.fetchone()
             search_result = "Nº INSCRIÇÃO: " + str(row[0]) + "\n" + "NOME: " + str(row[1]) + "\n" + "CURSO: " + str(row[2]) + "\n" + "SEMESTRE: " + str(row[3]) + "\n" + "TELEFONE" + str(row[4]) + "\n" + "ENDEREÇO" + str(row[5])
             QMessageBox.information(QMessageBox(), "Pesquisa realizada com sucesso", search_result)
@@ -67,9 +69,9 @@ class ConnectionDB():
             QMessageBox.warning(QMessageBox(), "Aluno não encontrado", "Contacte o administrador do sistema.")
 
     # Função responsável por deletar a informação do sistema.
-    def delete_one_student(self, number):
+    def delete_one_student(self, id):
         try:
-            result = self.db_cursor.execute("DELETE FROM students WHERE row =?", str(number)) # Consertada sintaxe // Não pode transformar em str o que é validado como int
+            result = self.db_cursor.execute("DELETE FROM students WHERE row =?", id) # Consertada sintaxe // Não pode transformar em str o que é validado como int
             self.conn.commit()
             self.db_cursor.close()
             self.conn.close()
